e6d9e67a8b990fb411a1ae379ae2d712b180fa58
sreedxm906 <54682329+sreedxm906@users.noreply.github.com>
2019-08-29 12:41:18 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Authors: Madisen Reed and Daniel Riedel
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Madisen Reed and Daniel Riedel\n");
+
+  return 0;
+}
